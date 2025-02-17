15e1e1d5f2a2c83fd6a65fa0f381f917867f441c
lslove10010 <byt16668@163.com>
2025-02-17 20:37:47 +0800

    初始化

--- application/admin/lang/zh-cn/play/zimu.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'Title' => '标题',
-    'Mid'   => '字幕id',
-    'Text'  => '字幕地址'
-];
--- public/assets/js/backend/play/jx.js
+++
@@ -1,9 +0,0 @@
-define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
-    var Controller = {
-        index: function () {
-            Form.api.bindevent($("form.edit-form"));
-        },
-
-    };
-    return Controller;
-});
